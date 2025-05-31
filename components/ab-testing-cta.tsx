71fb42c8e6ad7cd10864a0166b81b72484b9f415
thejasondev <jason.guerra1002@gmail.com>
2025-05-31 16:42:06 -0500

    mejora de boton

--- components/ab-testing-cta.tsx
+++ components/ab-testing-cta.tsx
@@ -49,8 +49,8 @@ export default function ABTestingCta({
           className="bg-marcelcar-highlight hover:bg-marcelcar-accent text-white py-6 sm:py-4 text-base w-full sm:w-auto"
           onClick={() => trackInteraction("call")}
         >
-          <Link href="/contacto">
-            <Phone className="mr-2 h-5 w-5" /> Solicitar Presupuesto
+          <Link href="https://wa.me/5491133333333?text=Hola,%20quiero%20contactar%20a%20Marcel%20Car%20para%20obtener%20un%20presupuesto">
+            <Phone className="mr-2 h-5 w-5" /> Contactar ahora
           </Link>
         </Button>
         <Button
